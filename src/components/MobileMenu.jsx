a1927bacaf8781bc352171bc045bf7e14786d1e5
Vishesh Tanwar <visheshtnwr@gmail.com>
2025-04-06 20:35:12 +0530

    fixed some issues.

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -35,11 +35,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         href="#about"
         onClick={() => setMenuOpen(false)}
         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
-            ${
-              menuOpen
-                ? "opacity-100 translate--0"
-                : "opacity-0 translate-y-5"
-            }
+            ${menuOpen ? "opacity-100 translate--0" : "opacity-0 translate-y-5"}
     `}
       >
         {" "}
@@ -49,11 +45,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         href="#projects"
         onClick={() => setMenuOpen(false)}
         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
-            ${
-              menuOpen
-                ? "opacity-100 translate--0"
-                : "opacity-0 translate-y-5"
-            }
+            ${menuOpen ? "opacity-100 translate--0" : "opacity-0 translate-y-5"}
     `}
       >
         {" "}
@@ -63,11 +55,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         href="#contact"
         onClick={() => setMenuOpen(false)}
         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
-            ${
-              menuOpen
-                ? "opacity-100 translate--0"
-                : "opacity-0 translate-y-5"
-            }
+            ${menuOpen ? "opacity-100 translate--0" : "opacity-0 translate-y-5"}
     `}
       >
         {" "}
