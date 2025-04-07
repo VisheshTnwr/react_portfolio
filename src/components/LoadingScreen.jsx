3219b970a56d5d34c90c5149aec90fc82cde18b3
Vishesh Tanwar <visheshtnwr@gmail.com>
2025-04-07 21:26:56 +0530

    commit

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -27,7 +27,7 @@ export const LoadingScreen = ({ onComplete }) => {
     <div className="fixed inset-0 z-50 bg-black tet-gray-100 flex flex-col items-center justify-center">
       <div className="mb-4 text-4xl font-mono font-bold">
         {" "}
-        {text} <span className="animate-blink ml-1"> | </span>{" "}
+        <span className="text-yellow-500">{text}</span> <span className="animate-blink ml-1 text-yellow-500"> | </span>{" "}
       </div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -7,38 +7,40 @@ export const Projects = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-      <div className="ma-w-5xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-yellow-500 to-orange-500 bg-clip-text text-transparent text-center ">
-          Featured Projects
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="p-6 rounded-xl border border-yellow-500/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-            <h3 className="text-xl font-bold mb-2">Radiant Computers</h3>
-            <p className="text-gray-400 mb-4">
-              Built a sleek and modern I.T website to showcase the services.
-            </p>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
-                <span
-                  key={key}
-                  className="bg-yellow-500/20 text-white py-1 px-3 rounded-full text-sm hover:bg-yellow-500/50 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+        <div className="ma-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-yellow-500 to-orange-500 bg-clip-text text-transparent text-center ">
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-yellow-500/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Radiant Computers</h3>
+              <p className="text-gray-400 mb-4">
+                Built a sleek and modern I.T website to showcase the services.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-yellow-500/20 text-white py-1 px-3 rounded-full text-sm hover:bg-yellow-500/50 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://www.radiantcomputers.com"
+                  target="_blank"
+                  className="text-yellow-400 hover:text-yellow-300 transition-colors my-4"
                 >
-                  {tech}
-                </span>
-              ))}
-            </div>
-            <div className="flex justify-between items-center">
-              <a
-                href="https://www.radiantcomputers.com"
-                className="text-yellow-400 hover:text-yellow-300 transition-colors my-4"
-              >
-                {" "}
-                View Project ¬
-              </a>
+                  {" "}
+                  
+                  View Project ¬
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </RevealOnScroll>
     </section>
   );
